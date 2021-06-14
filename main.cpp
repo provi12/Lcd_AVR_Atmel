c6526876b404e328e87a01dd3483886aeca5620c
Prashant Mishra <prashantmuniv@gmail.com>
2021-06-14 13:12:12 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+ * lcd_lib.cpp
+ *
+ * Created: 02-06-2021 16:46:22
+ * Author : Admin
+ */ 
+#define F_CPU 16000000UL	
+#include <avr/io.h>
+#include <util/delay.h>
+#include "lcd_lib.h"
+
+
+
+
+int main(void)
+{
+	LCD_Init();
+	LCD_String("hola");
+	while(1);
+	
+}
+
